05b42384b7d5ae0665f37e4510d0d310cb93c860
Karl Raf <karlforbusiness@gmail.com>
2018-05-15 15:02:05 +0200

    trash is ok, design is not up yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,14 +21,23 @@ class TasksController < ApplicationController
   end
 
   def update
+    if task_params[:completed] == 1
+      task_params[:completed] = "true"
+    end
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to task_path(@task)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
